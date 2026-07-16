3d3784fa2fcf793b514f56bb3c13142c0fba8a04
alexanderstevers15-maker <alexanderstevers15@gmail.com>
2026-07-15 23:13:23 -0400

    Create CartContext.jsx

---
+++ app/app/app/context/CartContext.jsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+
+const CartContext = createContext();
+
+
+
+export function CartProvider({children}) {
+
+
+const [cart,setCart] = useState([]);
+
+
+
+function addToCart(card){
+
+setCart([
+...cart,
+card
+]);
+
+}
+
+
+
+function removeFromCart(id){
+
+setCart(
+cart.filter(item => item.id !== id)
+);
+
+}
+
+
+
+return (
+
+<CartContext.Provider
+
+value={{
+cart,
+addToCart,
+removeFromCart
+}}
+
+>
+
+{children}
+
+</CartContext.Provider>
+
+);
+
+}
+
+
+
+export function useCart(){
+
+return useContext(CartContext);
+
+}
