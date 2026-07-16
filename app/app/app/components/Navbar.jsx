6b87ce4c6dcba34844c108fc399bf66a05a93c94
alexanderstevers15-maker <alexanderstevers15@gmail.com>
2026-07-15 23:03:22 -0400

    Create Navbar.jsx

---
+++ app/app/app/components/Navbar.jsx
@@ -0,0 +1,40 @@
+export default function Navbar() {
+
+  return (
+
+    <nav className="navbar">
+
+      <h1>
+        Stevers Sports Cards
+      </h1>
+
+
+      <div className="links">
+
+        <a href="/">
+          Home
+        </a>
+
+        <a href="/shop">
+          Shop
+        </a>
+
+        <a href="/about">
+          About
+        </a>
+
+        <a href="/contact">
+          Contact
+        </a>
+
+        <a href="/cart">
+          🛒 Cart
+        </a>
+
+      </div>
+
+    </nav>
+
+  );
+
+}
